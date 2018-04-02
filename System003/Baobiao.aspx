4afbf8784231a2b42711c6a457f8afb6db9bc730
徐绍宁 <wmdfnysdyy@126.com>
2018-04-02 10:09:30 +0800

    数据库连接测试——没有问题

--- System003/Baobiao.aspx
+++ System003/Baobiao.aspx
@@ -2,4 +2,6 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolderMain" runat="server">
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
 </asp:Content>
--- System003/Baobiao.aspx.cs
+++ System003/Baobiao.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace System003
 {
@@ -11,7 +13,29 @@ namespace System003
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
 
+                //创建数据库连接字符串
+                string SqlStr = "server=PC-201401242045;database=aspnetdb;uid=sa;pwd=ppzsppzs;";
+                SqlConnection con = new SqlConnection(SqlStr);  //创建数据库连接对象
+                
+                if (con.State == ConnectionState.Closed)            //判断连接是否关闭
+                {
+                    Label2.Text = "SQL Server数据库连接关闭！";
+                }
+                con.Open();                                 //打开数据库连接
+                if (con.State == ConnectionState.Open)          //判断连接是否打开
+                {
+                    Label1.Text = "SQL Server数据库连接开启！";
+                    con.Close();                                //关闭数据库连接
+                }
+            }
+
+            catch
+            {
+                Label2.Text = "GG";
+            }
         }
     }
 }
