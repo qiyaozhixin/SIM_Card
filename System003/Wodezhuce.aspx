99f5b5650f4cda0f53d596df8cd322efe734e662
徐绍宁 <wmdfnysdyy@126.com>
2018-04-09 13:15:41 +0800

    嗯，新的注册登录弄好了，接下来改调拨！

--- System003/Wodezhuce.aspx
+++ System003/Wodezhuce.aspx
@@ -2,4 +2,22 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolderMain" runat="server">
+    <asp:Label ID="Label1" runat="server" Text="员工OA"></asp:Label>
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="密码"></asp:Label>
+    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label4" runat="server" Text="ERP编号"></asp:Label>
+    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label3" runat="server" Text="所在部门"></asp:Label>
+    <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True">
+        <asp:ListItem>A部门</asp:ListItem>
+        <asp:ListItem>B部门</asp:ListItem>
+        <asp:ListItem>C部门</asp:ListItem>
+        <asp:ListItem>D部门</asp:ListItem>
+    </asp:DropDownList>
+    <br />
+    <asp:Button ID="Button8" runat="server" Text="注册" OnClick="Button8_Click" />
 </asp:Content>
--- System003/Wodezhuce.aspx.cs
+++ System003/Wodezhuce.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +14,27 @@ namespace System003
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection sqlcon = new SqlConnection("server=PC-201401242045;database=aspnetdb;uid=sa;pwd=ppzsppzs;");//创建数据库连接对象
+                if (sqlcon.State == ConnectionState.Closed)     //判断连接是否关闭
+                {
+                    sqlcon.Open();                              //打开数据库连接
+                }
+                SqlCommand sqlcmd = new SqlCommand("insert into aspnet_Yuangongtest values('" + TextBox1.Text + "','" + TextBox3.Text + "','" + DropDownList1.Text + "','" + TextBox2.Text + "',null)", sqlcon);
+                SqlDataReader sqldr = sqlcmd.ExecuteReader();
+                Response.Write("<script>window.alert('注册成功!');location.href='Default.aspx';</script>");
+                sqldr.Close();//关闭SqlDataReader对象
+                sqlcon.Close();//关闭数据库连接
+            }
+            catch
+            {
+                Response.Write("<script>window.alert('注册失败,此员工已完成注册!');location.href='Wodezhuce.aspx';</script>");
+            }
         }
     }
 }
