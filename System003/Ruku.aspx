8ca10afbdb11dd394e3b0940562b65cff2cd2ea9
徐绍宁 <wmdfnysdyy@126.com>
2018-04-12 11:36:14 +0800

    入库完成，赶紧加强PPT！

--- System003/Ruku.aspx
+++ System003/Ruku.aspx
@@ -2,4 +2,11 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolderMain" runat="server">
+    <asp:Label ID="Label4" runat="server" Text="ICCID"></asp:Label>
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label5" runat="server" Text="PUK码"></asp:Label>
+    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    <br />
+    <asp:Button ID="Button11" runat="server" Text="确定" OnClick="Button11_Click" />
 </asp:Content>
--- System003/Ruku.aspx.cs
+++ System003/Ruku.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +14,28 @@ namespace System003
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button11_Click(object sender, EventArgs e)
+        {
+            Session["iccid_ruku"] = TextBox1.Text;
+            Session["PUKma_ruku"] = TextBox2.Text;
+
+            SqlConnection sqlcon = new SqlConnection("server=PC-201401242045;database=aspnetdb;uid=sa;pwd=ppzsppzs;");//创建数据库连接对象                                                                                                                     //创建SqlCommand对象
+            SqlCommand sqlcmd = new SqlCommand("insert into aspnet_Cardtest(ICCID,当前库位,PUK码,卡状态) values ('" + Session["iccid_ruku"] + "','" + Session["dangqiandenglu"] + "','" + Session["PUKma_ruku"] + "','0');", sqlcon);
+            if (sqlcon.State == ConnectionState.Closed)     //判断连接是否关闭
+            {
+                sqlcon.Open();                              //打开数据库连接
+            }
+            //使用ExecuteReader方法的返回值创建SqlDataReader对象
+            SqlDataReader sqldr = sqlcmd.ExecuteReader();
+            sqldr.Close();//关闭SqlDataReader对象
+            sqlcon.Close();//关闭数据库连接
+
+            Session.Remove("iccid_ruku");
+            Session.Remove("PUKma_ruku");
+
+            Response.Write("<script>window.alert('入库成功！');location.href='Ruku.aspx';</script>");
         }
     }
 }
