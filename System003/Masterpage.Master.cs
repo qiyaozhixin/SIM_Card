54c706e6d88c32df982f2f89eb06d4e56a2c7517
徐绍宁 <wmdfnysdyy@126.com>
2018-04-15 11:02:47 +0800

    初步更改了登录时的权限

--- System003/Masterpage.Master.cs
+++ System003/Masterpage.Master.cs
@@ -13,26 +13,51 @@ namespace System003
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Session["quanxian"] = 0;
             SqlConnection sqlcon = new SqlConnection("server=PC-201401242045;database=aspnetdb;uid=sa;pwd=ppzsppzs;");//创建数据库连接对象                                                                                                                     //创建SqlCommand对象
-            SqlCommand sqlcmd = new SqlCommand("select OA from aspnet_Dengluzhe", sqlcon);
+            SqlCommand sqlcmd = new SqlCommand("select OA from aspnet_Dengluzhe;select 权限 from aspnet_Dengluzhe", sqlcon);
             if (sqlcon.State == ConnectionState.Closed)     //判断连接是否关闭
             {
                 sqlcon.Open();                              //打开数据库连接
             }
             //使用ExecuteReader方法的返回值创建SqlDataReader对象
-            Label2.Text = string.Format("{0}", sqlcmd.ExecuteScalar());
-            Session["dangqiandenglu"] = string.Format("{0}", sqlcmd.ExecuteScalar());
+            //Label2.Text = string.Format("{0}", sqlcmd.ExecuteScalar());
+            //Session["dangqiandenglu"] = string.Format("{0}", sqlcmd.ExecuteScalar());
             SqlDataReader sqldr = sqlcmd.ExecuteReader();
+            do
+            {
+                if (sqldr.HasRows)
+                {
+                    while (sqldr.Read())
+                    {
+                        Label2.Text += sqldr[0];
+                        Session["dangqiandenglu"] = Label2.Text;
+                    }
+                }
+            }
+            while (sqldr.NextResult() == false);
+            if (sqldr.HasRows)
+            {
+                while (sqldr.Read())
+                {
+                    Session["quanxian"] = sqldr[0];
+                }
+            }
             Button8.Visible = false;
             Label1.Visible = true;
             Button7.Visible = true;
-            //Label3.Visible = false;
-            if (sqldr.Read())//有登陆
+            if (int.Parse(Session["quanxian"].ToString()) == 3)//有登陆
             {
-                //Label2.Text += sqldr[0];
-                //Label2.Text = string.Format("{0}", sqlcmd.ExecuteScalar());
                 Button8.Visible = true;
-                //Button7.Visible = false;
+            }
+            else if(int.Parse(Session["quanxian"].ToString()) == 2)
+            {
+                Button8.Visible = true;
+            }
+            else if (int.Parse(Session["quanxian"].ToString()) == 1)
+            {
+                Button8.Visible = true;
+                Button10.Visible = false;
             }
             else//还未登陆
             {
@@ -62,7 +87,8 @@ namespace System003
             SqlDataReader sqldr = sqlcmd.ExecuteReader();
             sqldr.Close();//关闭SqlDataReader对象
             sqlcon.Close();//关闭数据库连接
-            //System.Threading.Thread.Sleep(3000);
+            Session.Remove("dangqiandenglu");
+            Session.Remove("quanxian");
             Response.Redirect("Wodedenglu.aspx");
         }
 
